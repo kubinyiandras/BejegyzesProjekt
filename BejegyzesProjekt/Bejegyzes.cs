4fc67dd0895684de2b61e4d25ee2cc26028fba17
Petrik Lajos Szg <info@petrik.hu>
2018-09-28 15:21:31 +0200

    elso feladat

---
+++ BejegyzesProjekt/Bejegyzes.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BejegyzesProjekt
+{
+    class Bejegyzes
+    {
+        string szerzo;
+        string tartalom;
+        int likok;
+        DateTime letrejott;
+        DateTime szerkesztve;
+
+        public Bejegyzes(string szerzo,string tartalom)
+        {
+            this.szerzo = szerzo;
+            this.tartalom = tartalom;
+            this.likok = 0;
+            this.letrejott =DateTime.Now;
+            this.szerkesztve = DateTime.Now;
+        }
+        public string Szerzo
+        {
+            get
+            {
+                return szerzo;
+            }
+        }
+        public string Tartalom
+        {
+            get
+            {
+                return tartalom;
+            }
+            set
+            {
+                szerkesztve = DateTime.Now;
+                tartalom = value;
+            }
+        }
+        public int Likok
+        {
+            get
+            {
+                return likok;
+            }
+        }
+        public DateTime Letrejott
+        {
+            get
+            {
+                return letrejott;
+            }
+        }
+        public DateTime Szerkeztve
+        {
+            get
+            {
+                return szerkesztve;
+            }
+        }
+        public void Like()
+        {
+            likok++;
+        }
+        public string Kiir()
+        {
+            string s = szerzo + "-" + likok + "-" + "\n Szerkeztve: " + szerkesztve + "\n" + tartalom;
+            return s;
+        }
+    
+
+    }
+   
+
+}
---
+++ BejegyzesProjekt/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BejegyzesProjekt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Bejegyzes> lista1 = new List<Bejegyzes>();
+            List<Bejegyzes> lista2 = new List<Bejegyzes>();
+
+            Console.WriteLine("Kérelek adja meg a darabszámot: ");
+            int db = Console.Read();
+            for (int i = 0; i < db; i++)
+            {
+                Console.WriteLine("adj egy szerzőt: ");
+                string szerzo = Console.ReadLine();
+                Console.WriteLine("adj egy tartalmat: ");
+                string tartalom = Console.ReadLine();
+                Bejegyzes b = new Bejegyzes(szerzo, tartalom);
+                lista1.Add(b);
+            }
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
